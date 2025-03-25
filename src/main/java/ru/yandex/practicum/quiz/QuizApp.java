f2c118bd36aa2427b0072f24162ccd718ccf742d
Integra <Integra4220@yandex.ru>
2025-03-25 17:15:57 +0300

    Привязываем свойства к классам

--- src/main/java/ru/yandex/practicum/quiz/QuizApp.java
+++ src/main/java/ru/yandex/practicum/quiz/QuizApp.java
@@ -1,22 +1,20 @@
 package ru.yandex.practicum.quiz;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import ru.yandex.practicum.quiz.model.QuizLog;
 import ru.yandex.practicum.quiz.service.ConsoleUI;
 import ru.yandex.practicum.quiz.service.ReportGenerator;
 
 @SpringBootApplication
+@RequiredArgsConstructor // Lombok автоматически создаст конструктор для final полей
+@ConfigurationPropertiesScan
 public class QuizApp implements CommandLineRunner {
-    private final ConsoleUI ui;
-    private final ReportGenerator reportGenerator;
-
-    // Явный конструктор для Spring
-    public QuizApp(ConsoleUI ui, ReportGenerator reportGenerator) {
-        this.ui = ui;
-        this.reportGenerator = reportGenerator;
-    }
+    private final ConsoleUI ui; // Внедряемый компонент
+    private final ReportGenerator reportGenerator; // Внедряемый компонент
 
     public static void main(String[] args) {
         SpringApplication.run(QuizApp.class, args);
@@ -24,7 +22,7 @@ public class QuizApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        QuizLog log = ui.startQuiz();
-        reportGenerator.generate(log);
+        QuizLog log = ui.startQuiz(); // Запускаем тест через ConsoleUI
+        reportGenerator.generate(log); // Генерируем отчет через ReportGenerator
     }
 }
